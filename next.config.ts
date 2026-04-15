157fe82c1c17f700dfb6d4821f1e5ea2b801ee52
toqaewa <kath.tokaeva@gmail.com>
2026-04-15 16:11:21 +0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,9 @@
 import type { NextConfig } from "next";
 
-const isProduction = process.env.NODE_ENV === "production";
-const repositoryName = process.env.GITHUB_REPOSITORY?.split("/")[1];
-const envBasePath = process.env.NEXT_PUBLIC_BASE_PATH;
-const basePath = envBasePath
-  ? `/${envBasePath.replace(/^\/+|\/+$/g, "")}`
-  : isProduction && repositoryName
-    ? `/${repositoryName}`
-    : "";
+// const isProduction = process.env.NODE_ENV === "production";
+// const repositoryName = process.env.GITHUB_REPOSITORY?.split("/")[1];
+// const envBasePath = process.env.NEXT_PUBLIC_BASE_PATH;
+// const basePath = envBasePath
 
 const nextConfig: NextConfig = {
   output: 'export',
@@ -17,8 +13,8 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath,
-  assetPrefix: basePath ? `${basePath}/` : '',
+  // basePath,
+  // assetPrefix: basePath ? `${basePath}/` : '',
   trailingSlash: true,
 };
 
